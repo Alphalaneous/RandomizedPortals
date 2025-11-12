23d12aab262f0a8f89ebbf40988be25612c1772f
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2025-11-12 16:40:08 -0500

    Crazy stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
+#include "Geode/loader/Loader.hpp"
+#include "Geode/loader/Log.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
+#include <Geode/binding/PlayLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJGameLevel.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/modify/EffectGameObject.hpp>
+#include <geode.custom-keybinds/include/Keybinds.hpp>
 #include <random>
 
 using namespace geode::prelude;
+using namespace keybinds;
 
 static constexpr std::array<int, 5> s_speedPortals = {
     200, 201, 202, 203, 1334
@@ -15,6 +21,43 @@ static int random(int min, int max) {
     static std::mt19937 rng{ std::random_device{}() };
     std::uniform_int_distribution<int> dist(min, max);
     return dist(rng);
+}
+
+void setupRandomSpeedsPre(GJBaseGameLayer* gjbgl) {
+    // only seems to work for particles that aren't preloaded
+    for (auto obj : CCArrayExt<GameObject*>(gjbgl->m_objects)) {
+        if (std::ranges::find(s_speedPortals, obj->m_objectID) != s_speedPortals.end()) {
+            if (!obj->getUserObject("original-id"_spr)) {
+                obj->setUserObject("original-id"_spr, CCInteger::create(obj->m_objectID));
+            }
+            obj->m_objectID = s_speedPortals[random(0, s_speedPortals.size() - 1)];
+            std::string frame = ObjectToolbox::sharedState()->intKeyToFrame(obj->m_objectID);
+            std::string particleFrameName = utils::string::replace(frame, "_001.png", "");
+            std::string particleString = fmt::format("{}{}_effect.plist-22", obj->m_objectID, particleFrameName);
+            obj->m_particleString = particleString;
+        }
+    }
+}
+
+void setupRandomSpeedsPost(GJBaseGameLayer* gjbgl) {
+    // somewhat evil hack
+    queueInMainThread([self = Ref(gjbgl)] {
+        queueInMainThread([self] {
+            for (auto obj : CCArrayExt<GameObject*>(self->m_objects)) {
+                if (std::ranges::find(s_speedPortals, obj->m_objectID) != s_speedPortals.end()) {
+                    std::string frame = ObjectToolbox::sharedState()->intKeyToFrame(obj->m_objectID);
+                    CCSprite* spr = CCSprite::createWithSpriteFrameName(frame.c_str());
+                    obj->setTextureRect(spr->getTextureRect(), spr->isTextureRectRotated(), spr->getTextureRect().size);
+                    if (obj->m_glowSprite) {
+                        std::string glowFrame = utils::string::replace(frame, "_001.png","_glow_001.png");
+                        CCSprite* glowSpr = CCSprite::createWithSpriteFrameName(glowFrame.c_str());
+                        obj->m_glowSprite->setTextureRect(glowSpr->getTextureRect(), glowSpr->isTextureRectRotated(), glowSpr->getTextureRect().size);
+                    }
+                    obj->updateOrientedBox();
+                }
+            }
+        });
+    });
 }
 
 class $modify(GJGameLevel) {
@@ -68,10 +111,12 @@ class $modify(MyPlayLayer, PlayLayer) {
 
     void togglePracticeMode(bool practiceMode) {
         PlayLayer::togglePracticeMode(practiceMode);
-        updateOpacity();
+        if (Mod::get()->getSettingValue<bool>("soft-toggle")) {
+            updateOpacity();
+        }
     }
 
-    void updateSpeed(GameObject* object = nullptr) {
+    void updateSpeed(GameObject* object = nullptr, bool fromCheckpoint = false) {
         setupSpeedNode();
         auto fields = m_fields.self();
         if (!fields->m_speedNode) return;
@@ -99,24 +144,26 @@ class $modify(MyPlayLayer, PlayLayer) {
                 objectID = 1334;
             }
 
-            auto startSpeedOrig = m_levelSettings->m_startSpeed;
+            if (!fromCheckpoint) {
+                auto startSpeedOrig = m_levelSettings->m_startSpeed;
 
-            switch (startSpeedOrig) {
-                case Speed::Slow:
-                    originalID = 200;
-                    break;
-                case Speed::Normal:
-                    originalID = 201;
-                    break;
-                case Speed::Fast:
-                    originalID = 202;
-                    break;
-                case Speed::Faster:
-                    originalID = 203;
-                    break;
-                case Speed::Fastest:
-                    originalID = 1334;
-                    break;
+                switch (startSpeedOrig) {
+                    case Speed::Slow:
+                        originalID = 200;
+                        break;
+                    case Speed::Normal:
+                        originalID = 201;
+                        break;
+                    case Speed::Fast:
+                        originalID = 202;
+                        break;
+                    case Speed::Faster:
+                        originalID = 203;
+                        break;
+                    case Speed::Fastest:
+                        originalID = 1334;
+                        break;
+                }
             }
         }
         else {
@@ -130,15 +177,17 @@ class $modify(MyPlayLayer, PlayLayer) {
         CCSprite* spr = CCSprite::createWithSpriteFrameName(frame.c_str());
         spr->setAnchorPoint({0.f, 0.f});
 
-        std::string frameOrig = ObjectToolbox::sharedState()->intKeyToFrame(originalID);
-        CCSprite* sprOrig = CCSprite::createWithSpriteFrameName(frameOrig.c_str());
-        sprOrig->setScale(0.4f);
-        sprOrig->setAnchorPoint({0.f, 0.f});
+        if (!fromCheckpoint) {
+            std::string frameOrig = ObjectToolbox::sharedState()->intKeyToFrame(originalID);
+            CCSprite* sprOrig = CCSprite::createWithSpriteFrameName(frameOrig.c_str());
+            sprOrig->setScale(0.4f);
+            sprOrig->setAnchorPoint({0.f, 0.f});
 
-        sprOrig->setPositionX(spr->getContentWidth() + 5);
+            sprOrig->setPositionX(spr->getContentWidth() + 5);
+            fields->m_speedNode->addChild(sprOrig);
+        }
 
         fields->m_speedNode->addChild(spr);
-        fields->m_speedNode->addChild(sprOrig);
 
         updateOpacity();
     }
@@ -158,32 +207,56 @@ class $modify(MyPlayLayer, PlayLayer) {
         }
         PlayLayer::levelComplete();
     }
+
+    void resetLevel() {
+        PlayLayer::resetLevel();
+        if (Mod::get()->getSettingValue<bool>("soft-toggle")) {
+            auto fields = m_fields.self();
+            if (m_isPracticeMode && getCurrentPercent() > 0) {
+                updateSpeed(nullptr, true);
+            }
+        }
+    }
+
+    static MyPlayLayer* get() {
+        return static_cast<MyPlayLayer*>(PlayLayer::get());
+    }
 };
 
 class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 
-    void setupLevelStart(LevelSettingsObject* p0) {
-        if (!Mod::get()->getSettingValue<bool>("soft-toggle")) return GJBaseGameLayer::setupLevelStart(p0);
+    struct Fields {
+        bool m_setAtStart = false;
+        bool m_firstSetup = true;
+        int m_startSpeed = -1;
+    };
 
-        // only seems to work for particles that aren't preloaded
-        for (auto obj : CCArrayExt<GameObject*>(m_objects)) {
-            if (std::ranges::find(s_speedPortals, obj->m_objectID) != s_speedPortals.end()) {
-                if (!obj->getUserObject("original-id"_spr)) {
-                    obj->setUserObject("original-id"_spr, CCInteger::create(obj->m_objectID));
-                }
-                obj->m_objectID = s_speedPortals[random(0, s_speedPortals.size() - 1)];
-                std::string frame = ObjectToolbox::sharedState()->intKeyToFrame(obj->m_objectID);
-                std::string particleFrameName = utils::string::replace(frame, "_001.png", "");
-                std::string particleString = fmt::format("{}{}_effect.plist-22", obj->m_objectID, particleFrameName);
-                obj->m_particleString = particleString;
-            }
+    void resetRandom() {
+        auto fields = m_fields.self();
+        auto playLayer = PlayLayer::get();
+
+        setupRandomSpeedsPre(this);
+
+        playLayer->resetLevel();
+
+        setupRandomSpeedsPost(this);
+
+        if (playLayer->getCurrentPercent() == 0) {
+            fields->m_startSpeed = -1;
+            setupRandomStartSpeed();
+        }
+    }
+
+    void setupRandomStartSpeed() {
+        auto fields = m_fields.self();
+
+        float playerSpeed = 0.9f;
+    
+        if (fields->m_startSpeed == -1) {
+            fields->m_startSpeed = random(0, 4);
         }
 
-        GJBaseGameLayer::setupLevelStart(p0);
-
-        int speed = random(0, 4);
-        float playerSpeed = 0.9f;
-        switch (speed) {
+        switch (fields->m_startSpeed) {
             case 1: {
                 playerSpeed = 0.70f;
                 break;
@@ -209,42 +282,80 @@ class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
         m_player1->m_playerSpeed = playerSpeed;
         if (m_player2) m_player2->m_playerSpeed = playerSpeed;
 
-
-        if (auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get())) {
+        if (auto playLayer = MyPlayLayer::get()) {
             playLayer->updateSpeed();
         }
+    }
 
-        // somewhat evil hack
-        queueInMainThread([self = Ref(this)] {
-            queueInMainThread([self] {
-                for (auto obj : CCArrayExt<GameObject*>(self->m_objects)) {
-                    if (std::ranges::find(s_speedPortals, obj->m_objectID) != s_speedPortals.end()) {
-                        std::string frame = ObjectToolbox::sharedState()->intKeyToFrame(obj->m_objectID);
-                        CCSprite* spr = CCSprite::createWithSpriteFrameName(frame.c_str());
-                        obj->setTextureRect(spr->getTextureRect(), spr->isTextureRectRotated(), spr->getTextureRect().size);
-                        if (obj->m_glowSprite) {
-                            std::string glowFrame = utils::string::replace(frame, "_001.png","_glow_001.png");
-                            CCSprite* glowSpr = CCSprite::createWithSpriteFrameName(glowFrame.c_str());
-                            obj->m_glowSprite->setTextureRect(glowSpr->getTextureRect(), glowSpr->isTextureRectRotated(), glowSpr->getTextureRect().size);
-                        }
-                        obj->updateOrientedBox();
-                    }
-                }
-            });
-        });
+    void setupLevelStart(LevelSettingsObject* p0) {
+        auto playLayer = MyPlayLayer::get();
+        if (!Mod::get()->getSettingValue<bool>("soft-toggle") || !playLayer) return GJBaseGameLayer::setupLevelStart(p0);
+
+        auto fields = m_fields.self();
+
+        if (fields->m_firstSetup) {
+            fields->m_firstSetup = false;
+            return GJBaseGameLayer::setupLevelStart(p0);
+        }
+
+        if (!fields->m_setAtStart) {
+            fields->m_setAtStart = true;
+
+            setupRandomStartSpeed();
+            setupRandomSpeedsPre(this);
+
+            GJBaseGameLayer::setupLevelStart(p0);
+
+            setupRandomSpeedsPost(this);
+        }
+        else {
+            GJBaseGameLayer::setupLevelStart(p0);
+            if (playLayer->getCurrentPercent() == 0) {
+                setupRandomStartSpeed();
+            }
+        }
+    }
+
+    static MyGJBaseGameLayer* get() {
+        if (PlayLayer::get()) return static_cast<MyGJBaseGameLayer*>(GJBaseGameLayer::get());
+        return nullptr;
     }
 };
+
+$execute {
+    BindManager::get()->registerBindable({
+        "reset-random"_spr,
+        "Reset Random Portals",
+        "",
+        { Keybind::create(KEY_T, Modifier::None) },
+        "",
+        false
+    });
+
+    new EventListener(+[](InvokeBindEvent* event) {
+        if (Mod::get()->getSettingValue<bool>("soft-toggle") && event->isDown()) {
+            if (auto gjbgl = MyGJBaseGameLayer::get()) {
+                gjbgl->resetRandom();
+            }
+        }
+        return ListenerResult::Propagate;
+    }, InvokeBindFilter(nullptr, "reset-random"_spr));
+
+}
 
 class $modify(EffectGameObject) {
 
     void triggerObject(GJBaseGameLayer* p0, int p1, gd::vector<int> const* p2) {
         EffectGameObject::triggerObject(p0, p1, p2);
 
-        if (std::ranges::find(s_speedPortals, m_objectID) != s_speedPortals.end()) {
-            if (auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get())) {
-                playLayer->updateSpeed(this);
+        if (!PlayLayer::get()) return;
+
+        if (Mod::get()->getSettingValue<bool>("soft-toggle")) {
+            if (std::ranges::find(s_speedPortals, m_objectID) != s_speedPortals.end()) {
+                if (auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get())) {
+                    playLayer->updateSpeed(this);
+                }
             }
         }
-
     }
 };
